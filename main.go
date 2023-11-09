1737c7b3f9d753219ed3ced4c2f8a67d38b4ff6c
Wes Warriner <fridgehistory@gmail.com>
2023-11-09 15:47:55 -0700

    update semantics to match ultimate functionality of this tool; add html rendering to main

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    // "fmt"
+    "fmt"
     "math/rand"
     "net/http"
     "time"
 )
 
 // struct to contain urls, which is simply a map containing 
-// shortened keys as keys and original URLs as values
-type URLShortener struct {
+// enhancedKeys as keys and original URLs as values
+type URLEnhancer struct {
     urls map[string]string
 }
 
-// Method of URLShortner struct to actually perform the shortening.
+// Method of URLEnhance struct to actually perform the enhancing.
 // args: http response writer, http request
-func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
+func (us *URLEnhancer) HandleEnhance(w http.ResponseWriter, r *http.Request) {
 	// if request method is not POST, error out
 	if r.Method != http.MethodPost {
 		http.Error(w, "Inbalid request method", http.StatusMethodNotAllowed)
@@ -29,12 +29,30 @@ func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// generate a unique shortened key for the original URL
-	shortKey := generateShortKey()
-	us.urls[shortKey] = originalURL
+	// generate a unique enhanced key for the original URL
+	enhancedKey := generateShortKey()
+	// assign item in urls map, enhancedKey: originalURL
+	us.urls[enhancedKey] = originalURL
+
+	// Construct the full enhanced URL
+	// TODO: replace with live version
+	enhancedURL := fmt.Sprintf("http://localhost:8080/better/%s", enhancedKey)
+
+	// Render the HTML response with the enhanced URL
+	w.Header().Set("Content-Type", "text/html")
+	responseHTML := fmt.Sprintf(`
+		<h1>gorter URL Enhancer</h1>
+	        <p>Original URL: %s</p>
+        	<p>Better URL: <a href="%s">%s</a></p>
+        	<form method="post" action="/enhanced">
+            		<input type="text" name="url" placeholder="Enter a URL">
+            		<input type="submit" value="Enhance">
+        	</form>
+	`, originalURL, enhancedURL, enhancedURL)
+	fmt.Fprintf(w, responseHTML)
 }
 
-// function to create a new unique short key for the original URL
+// function to create a new unique enhanced key for the original URL
 func generateShortKey() string {
 	// alphanumerics
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -49,14 +67,14 @@ func generateShortKey() string {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	// make allocates memory on the heap, slotting zero-values.
 	// in this case a bytes slices (not array, mind you) of length keyLength
-	shortKey := make([]byte, keyLength)
+	enhancedKey := make([]byte, keyLength)
 	// then for each element in the slice put in 
 	// the character from charset at the index 
 	// that is a pseudo-random number in the
 	// interval [0, len(charset))
-	for i := range shortKey {
-		shortKey[i] = charset[rand.Intn(len(charset))]
+	for i := range enhancedKey {
+		enhancedKey[i] = charset[rand.Intn(len(charset))]
 	}
-	// and return the filled byte slice shortKey as (joined) a string
-	return string(shortKey)
+	// and return the filled byte slice enhancedKey as (joined) a string
+	return string(enhancedKey)
 }
