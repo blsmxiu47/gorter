2e2e386a296c4dda5ced529e98d0c7489e12c0c1
Wes Warriner <fridgehistory@gmail.com>
2023-11-09 15:34:26 -0700

    begin coding HandleShorten() method; adapt generateShortKey() with comments

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
     // "fmt"
-    // "math/rand"
+    "math/rand"
     "net/http"
-    // "time"
+    "time"
 )
 
 // struct to contain urls, which is simply a map containing 
@@ -13,8 +13,50 @@ type URLShortener struct {
     urls map[string]string
 }
 
-// function to actually perform the shortening
-// TODO: further comments for LEARNING
+// Method of URLShortner struct to actually perform the shortening.
+// args: http response writer, http request
 func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	// if request method is not POST, error out
+	if r.Method != http.MethodPost {
+		http.Error(w, "Inbalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// if url from request is missing, error out
+	originalURL := r.FormValue("url")
+	if originalURL == "" {
+		http.Error(w, "URL parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	// generate a unique shortened key for the original URL
+	shortKey := generateShortKey()
+	us.urls[shortKey] = originalURL
+}
+
+// function to create a new unique short key for the original URL
+func generateShortKey() string {
+	// alphanumerics
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	// sets length of the key. longer makes uniqueness more likely
+	// with 62 characters, N_possibilities := 62**keyLength
+	// with keyLength of 8, there are > 2.18e14 possiblities 
+	const keyLength = 8
+
+	// New generates a new Rand struct.
+	// NewSource returns a new psuedo-random Source seeded with the given value.
+	// our seed is the current time in seconds in the Unix epoch
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	// make allocates memory on the heap, slotting zero-values.
+	// in this case a bytes slices (not array, mind you) of length keyLength
+	shortKey := make([]byte, keyLength)
+	// then for each element in the slice put in 
+	// the character from charset at the index 
+	// that is a pseudo-random number in the
+	// interval [0, len(charset))
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+	}
+	// and return the filled byte slice shortKey as (joined) a string
+	return string(shortKey)
 }
