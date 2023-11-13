4e5db8ee2406505de2fa0bd6e3b5c8f354311b98
Wes Warriner <fridgehistory@gmail.com>
2023-11-12 17:08:03 -0700

    write functions to handle API requests and shuffle url key strings for creating the 'enhanced' new urls; general naming conventions and code cleanup

--- main.go
+++ main.go
@@ -1,34 +1,104 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "math/rand"
-    "net/http"
-    "time"
+	"fmt"
+	"html/template"
+	"io"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 )
+
+// type for our REST API respose(s)
+type Response struct {
+	Name string `json:"name"`
+}
 
 // struct to contain urls, which is simply a map containing 
 // enhancedKeys as keys and original URLs as values
-type UrlMap struct {
+type URLMap struct {
     urls map[string]string
 }
 
 // struct for injecting variables into template HTML
 type templateUpdate struct {
 	enhanced bool
-	enhancedUrl string
+	enhancedURL string
 }
 
-// Method of URLEnhance struct to actually perform the enhancing.
+// At this moment there are 1017 apparently.. 
+// Somehow seems like more than I remember..
+// This must be what it feels like to get old
+const MAX_SPECIES_ID = 1017
+
+// Function to GET a single pokemon species name from 
+// the PokeAPI: https://pokeapi.co/docs/v2
+func getSpeciesName(species_id int) string {
+	client := &http.Client{}
+	
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://pokeapi.co/api/v2/pokemon-species/%d"), nil)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	
+	req.Header.Add("Accept", "application/json")
+ 	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	var responseObject Response
+	json.Unmarshal(body, &responseObject)
+	// TODO: remove print
+	fmt.Printf("Name returned: %s", responseObject.Name)
+	
+	return responseObject.Name
+}
+
+// Function to insert anagramized pokemon species name into an 
+// existing string and shuffle
+func enhanceText(text string) string {
+	// Get pseudo-random name from the API
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	name := []byte(getSpeciesName(rand.Intn(MAX_SPECIES_ID)))
+
+	// Concat the text and name as byte slice (doesn't matter where 
+	// name get inserted was this whole thing will get shuffled below)
+	b := append(name, text[:]...)
+	// Shuffle the letters
+	// math//rand has a pseudo-random shuffling function
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	rand.Shuffle(len(b), func(i, j int) {
+		b[i], b[j] = b[j], b[i]
+	})
+	// and back to string
+	enhancedText := string(b)
+
+	return enhancedText
+}
+
+
+// Function to actually perform the URL shortening and spit out html
+// containing the new URL to the page
 // args: http response writer, http request
 // returns: HTML string to be rendered, including link to the enhanced URL
-func EnhanceUrl(inputUrl string) string {
+func enhanceURL(inputURL string) string {
 	// generate a unique enhanced key for the original URL
-	enhancedKey := generateShortKey()
+	shortKey := generateShortKey()
 	// enhance the shortKey
 	// TODO: implement enhanceShortKey()
-	// enhancedKey := enhanceShortKey(shortKey)
+	enhancedKey := enhanceText(shortKey)
 	// assign item in urls map, enhancedKey: originalURL
 
 	// Construct the full enhanced URL
@@ -43,8 +113,8 @@ func EnhanceUrl(inputUrl string) string {
 	return responseHTML
 }
 
-// UrlMap method to handle redirection from enhanced URL to original
-func (ue *UrlMap) HandleRedirect(w http.ResponseWriter, r *http.Request) {
+// URLMap method to handle redirection from enhanced URL to original
+func (ue *URLMap) handleRedirect(w http.ResponseWriter, r *http.Request) {
 	// get enhanced key portion of request url path
 	enhancedKey := r.URL.Path[len("/enhanced/"):]
 	// if empty string, error out
@@ -93,8 +163,8 @@ func generateShortKey() string {
 }
 
 func main() {
-	// pointer to UrlMap, allocating memory for urls
-	urlMap := &UrlMap{
+	// pointer to URLMap, allocating memory for urls
+	urlMap := &URLMap{
 		urls: make(map[string]string),
 	}
 	
@@ -108,16 +178,16 @@ func main() {
 		}
 
 		// grab URL from input form when entered by user
-		inputUrl := r.FormValue("url")
+		inputURL := r.FormValue("url")
 
 		// initiate URL enhancing
-		enhancedUrl := EnhanceUrl(inputUrl)
-		urlMap.urls[enhancedUrl] = inputUrl 
+		enhancedURL := enhanceURL(inputURL)
+		urlMap.urls[enhancedURL] = inputURL 
 
 		// fill in variables to insert into the web page
 		tmplUpdate := templateUpdate{
 			enhanced: true,
-			enhancedUrl: enhancedUrl,
+			enhancedURL: enhancedURL,
 		}
 		tmpl.Execute(w, tmplUpdate)
 	})
