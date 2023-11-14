a5c262855e3b137c0acd4e0038e82b15c6a424cd
Wes Warriner <fridgehistory@gmail.com>
2023-11-13 20:37:52 -0700

    refactor http handling functions and re-order in script

--- main.go
+++ main.go
@@ -33,12 +33,89 @@ type templateUpdate struct {
 // This must be what it feels like to get old
 const MAX_SPECIES_ID = 1017
 
-// Function to GET a single pokemon species name from 
+// App host name (and port, if applicable)
+const MY_HOST = "localhost:8080"
+
+// Actually perform the URL shortening and spit out html
+// containing the new URL to the page
+// args: http response writer, http request
+// returns: HTML string to be rendered, including link to the enhanced URL
+func (um *URLMap) handleEnhance(w http.ResponseWriter, r *http.Request) {
+	// if request method is not POST, error out
+	// TODO: dont send both GET and POST in the first place?
+	//if r.Method != http.MethodPost {
+	//	http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	//	return
+	//}
+
+	// if url from request is missing, error out
+	inputURL := r.FormValue("url")
+	if inputURL == "" {
+		http.Error(w, "URL parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("inputURL: %s", inputURL)
+
+	// generate a unique enhanced key for the original URL
+	shortKey := generateShortKey()
+	fmt.Printf("shortKey: %s\n", shortKey)
+
+	// enhance the shortKey
+	enhancedKey := enhanceText(shortKey)
+	fmt.Printf("enhancedKey: %s\n", enhancedKey)
+	// assign item in urls map, enhancedKey: originalURL
+	um.urls[enhancedKey] = inputURL
+
+	// Construct the full enhanced URL
+	enhancedURL := fmt.Sprintf("http://%s/enhanced/%s", MY_HOST, enhancedKey)
+	fmt.Printf("enhancedURL: %s\n", enhancedURL)
+
+	// Render the HTML response with the enhanced URL
+	//tmpl := template.Must(template.ParseFiles("templates/form.html"))
+	tmpl, err := template.ParseFiles("templates/form.html")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	// TODO: no error above, however tmpl not looking as expected here
+	fmt.Println(tmpl)
+	w.Header().Set("Content-Type", "text/html")
+	// Fill in variables to insert into the web page
+	tmplUpdate := templateUpdate{
+		enhanced: true,
+		enhancedURL: enhancedURL,
+	}
+	tmpl.Execute(w, tmplUpdate)
+}
+
+// URLMap method to handle redirection from enhanced URL to original
+func (um *URLMap) handleRedirect(w http.ResponseWriter, r *http.Request) {
+	// get enhanced key portion of request url path
+	enhancedKey := r.URL.Path[len("/enhance/"):]
+	// if empty string, error out
+	if enhancedKey == "" {
+		http.Error(w, "Enhanced key is missing", http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the original URL from the `urls` map using the enhanced key
+	originalURL, found := um.urls[enhancedKey]
+	// if not found in map, error out
+	if !found {
+		http.Error(w, "Enhanced key not found", http.StatusNotFound)
+		return
+	}
+
+	// and redirect the user to the original URL
+	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
+}
+
+// GET a single pokemon species name from 
 // the PokeAPI: https://pokeapi.co/docs/v2
 func getSpeciesName(species_id int) string {
 	client := &http.Client{}
 	
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://pokeapi.co/api/v2/pokemon-species/%d"), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://pokeapi.co/api/v2/pokemon-species/%d", species_id), nil)
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
@@ -61,12 +138,12 @@ func getSpeciesName(species_id int) string {
 	var responseObject Response
 	json.Unmarshal(body, &responseObject)
 	// TODO: remove print
-	fmt.Printf("Name returned: %s", responseObject.Name)
+	fmt.Printf("Name returned: %s\n", responseObject.Name)
 	
 	return responseObject.Name
 }
 
-// Function to insert anagramized pokemon species name into an 
+// Insert anagramized pokemon species name into an 
 // existing string and shuffle
 func enhanceText(text string) string {
 	// Get pseudo-random name from the API
@@ -88,54 +165,7 @@ func enhanceText(text string) string {
 	return enhancedText
 }
 
-
-// Function to actually perform the URL shortening and spit out html
-// containing the new URL to the page
-// args: http response writer, http request
-// returns: HTML string to be rendered, including link to the enhanced URL
-func enhanceURL(inputURL string) string {
-	// generate a unique enhanced key for the original URL
-	shortKey := generateShortKey()
-	// enhance the shortKey
-	// TODO: implement enhanceShortKey()
-	enhancedKey := enhanceText(shortKey)
-	// assign item in urls map, enhancedKey: originalURL
-
-	// Construct the full enhanced URL
-	// TODO: replace with live version
-	enhancedURL := fmt.Sprintf("http://localhost:8080/enhanced/%s", enhancedKey)
-
-	// Render the HTML response with the enhanced URL
-	responseHTML := fmt.Sprintf(`
-        	<p>Better URL: <a href="%s">%s</a></p>
-	`, enhancedURL, enhancedURL)
-
-	return responseHTML
-}
-
-// URLMap method to handle redirection from enhanced URL to original
-func (ue *URLMap) handleRedirect(w http.ResponseWriter, r *http.Request) {
-	// get enhanced key portion of request url path
-	enhancedKey := r.URL.Path[len("/enhanced/"):]
-	// if empty string, error out
-	if enhancedKey == "" {
-		http.Error(w, "Enhanced key is missing", http.StatusBadRequest)
-		return
-	}
-
-	// Retrieve the original URL from the `urls` map using the enhanced key
-	originalURL, found := ue.urls[enhancedKey]
-	// if not found in map, error out
-	if !found {
-		http.Error(w, "Enhanced key not found", http.StatusNotFound)
-		return
-	}
-
-	// and redirect the user to the original URL
-	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
-}
-
-// function to create a new unique enhanced key for the original URL
+// Create a new unique short key for the original URL
 func generateShortKey() string {
 	// alphanumerics
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -150,52 +180,39 @@ func generateShortKey() string {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	// make allocates memory on the heap, slotting zero-values.
 	// in this case a bytes slices (not array, mind you) of length keyLength
-	enhancedKey := make([]byte, keyLength)
+	shortKey := make([]byte, keyLength)
 	// then for each element in the slice put in 
 	// the character from charset at the index 
 	// that is a pseudo-random number in the
 	// interval [0, len(charset))
-	for i := range enhancedKey {
-		enhancedKey[i] = charset[rand.Intn(len(charset))]
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
 	}
-	// and return the filled byte slice enhancedKey as (joined) a string
-	return string(enhancedKey)
+	// and return the filled byte slice shortKey as (joined) a string
+	return string(shortKey)
 }
 
-func main() {
-	// pointer to URLMap, allocating memory for urls
-	urlMap := &URLMap{
-		urls: make(map[string]string),
-	}
-	
-	// base web app
-	tmpl := template.Must(template.ParseFiles("index.html"))
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
+func main() {
+	// base web app
+	//tmpl := template.Must(template.ParseFiles("index.html"))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			http.Redirect(w, r, "/enhance", http.StatusSeeOther)
 			return
 		}
-
-		// grab URL from input form when entered by user
-		inputURL := r.FormValue("url")
-
-		// initiate URL enhancing
-		enhancedURL := enhanceURL(inputURL)
-		urlMap.urls[enhancedURL] = inputURL 
-
-		// fill in variables to insert into the web page
-		tmplUpdate := templateUpdate{
-			enhanced: true,
-			enhancedURL: enhancedURL,
-		}
-		tmpl.Execute(w, tmplUpdate)
+		http.ServeFile(w, r, "index.html")
 	})
-	
-	// handle URL enhancing and redirection via methods defined above
-	// http.HandleFunc("/enhance/", enhancer.HandleRedirect)
+
+	urls := &URLMap{
+		urls: make(map[string]string),
+	}
+
+	mux.HandleFunc("/enhance", urls.handleEnhance)
+	mux.HandleFunc("/enhanced/", urls.handleRedirect)
 
 	// TODO: refactor for live app. using localhost now for testing
-	fmt.Println("URL Enhancer is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("URL Enhancer is running on http://%s\n", MY_HOST)
+	http.ListenAndServe(":8080", mux)
 }
